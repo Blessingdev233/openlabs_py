52cc67693225a2cf9a2ca3781e6c6afe7cd434fb
Blessingdev233 <blessing.lartey@gmail.com>
2024-07-11 18:58:21 +0000

    python lessons

---
+++ Lesson1.py
@@ -0,0 +1,18 @@
+print("Hello world")
+
+x = 10
+y = 20
+
+result = x+y
+
+print(result)
+
+
+
+Name = "Simon Peter"
+RegistrationNumber = 11223344
+DateOfBirth = 19
+
+print(Name)
+print(RegistrationNumber)
+print(DateOfBirth)
---
+++ Lesson2.py
@@ -0,0 +1,4 @@
+stydent_det = {"Name: Simon Peter", "Registration Number: 11122233", "Date of Birth: !982", "Address: 44 apple ln.", "City: Tema", "Home Phone number: 3310", "Score in Subject1: 89", "Score in Subject2: 98", "Average Score: 78"}
+
+for info in stydent_det:
+    print(info)
