23e2f6107eac27293da4c673c26db59a6af0960a
Blessingdev233 <blessing.lartey@gmail.com>
2024-07-23 17:12:25 +0000

    lesson 4

---
+++ Lesson4.py
@@ -0,0 +1,17 @@
+letters = ("a","b","c","d","e")
+
+print(letters)
+
+a = int(input("Enter the values for a: "))
+b = int(input("Enter the values for b: "))
+
+result = a*b
+
+print(result)
+
+a = float(input("Enter the value for a: "))
+b = float(input("Enter the value for b: "))
+
+result = a*b
+
+print(result)
